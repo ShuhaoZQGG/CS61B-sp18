395e33098da682e77cd2971d71694632cd7cbb9a
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-02-09 19:56:33 -0500

    implement addLast, getLast and size methods

--- lecture3/SLList.java
+++ lecture3/SLList.java
@@ -19,14 +19,51 @@ public class SLList {
     public void addFirst(int x) {
         first = new IntNode(x, first);
     }
+
     public int getFirst() {
         return first.item;
     }
+
+    public void addLast(int x) {
+        IntNode p = first;
+        while (p.next != null) {
+            p = p.next;
+        }
+
+        p.next = new IntNode(x, null);
+    }
+
+    public int getLast() {
+        IntNode p = first;
+        while(p.next != null) {
+            p = p.next;
+        }
+
+        return p.item;
+    }
+
+    private static int size(IntNode p) {
+        if (p.next == null) {
+            return 1;
+        }
+
+        return 1 + size(p.next);
+    }
+
+    public int size() {
+        return size(first);
+    }
+
     public static void main(String[] args) {
         /* Create a list of one integer, namely 10 */
 
         // Compared to InList: InList L = new InList(15, null);
         SLList L= new SLList(10);
+        L.addFirst(5);
+        L.addFirst(15);
+        L.addLast(20);
         System.out.println(L.getFirst());
+        System.out.println(L.getLast());
+        System.out.println(L.size());
     }
 }
