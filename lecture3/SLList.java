05ae9d1acad31cca1560bb84765c8d9d2637bb0f
Shuhao Zhang <shuhao.qgg.zhang@gmail.com>
2022-02-09 19:17:20 -0500

    implement IntNode and SLList for lecture 3

---
+++ lecture3/SLList.java
@@ -0,0 +1,22 @@
+/** An SLList is a list of the integers, which hides the terrible truth of the nakedness within*/
+public class SLList {
+    private IntNode first;
+
+    public SLList(int x) {
+        first = new IntNode(x, null);
+    }
+
+    public void addFirst(int x) {
+        first = new IntNode(x, first);
+    }
+    public int getFirst() {
+        return first.item;
+    }
+    public static void main(String[] args) {
+        /* Create a list of one integer, namely 10 */
+
+        // Compared to InList: InList L = new InList(15, null);
+        SLList L= new SLList(10);
+        System.out.println(L.getFirst());
+    }
+}
